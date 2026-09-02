d457829387f753d82ca00e32325e4a810e5a4649
Aincrad179 <aincrad1227@outlook.com>
2026-09-02 13:45:31 +0800

    delete sprint

--- Assets/Scripts/Core/Flame/ConsumeContext.cs
+++ Assets/Scripts/Core/Flame/ConsumeContext.cs
@@ -2,12 +2,10 @@ namespace ProjectEmber.Core
 {
     public readonly struct ConsumeContext
     {
-        public bool IsPlayerSprinting { get; }
         public float Oxygen { get; }
 
-        public ConsumeContext(bool isPlayerSprinting, float oxygen)
+        public ConsumeContext(float oxygen)
         {
-            IsPlayerSprinting = isPlayerSprinting;
             Oxygen = oxygen;
         }
     }
